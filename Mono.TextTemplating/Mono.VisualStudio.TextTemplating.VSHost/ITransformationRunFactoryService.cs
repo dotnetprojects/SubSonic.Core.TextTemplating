0d643b3cc8293720587cd28b8d8befab47dc1715
Kenneth Carter <kccarter32@gmail.com>
2020-08-04 17:19:41 -0600

    no message

--- Mono.TextTemplating/Mono.VisualStudio.TextTemplating.VSHost/ITransformationRunFactoryService.cs
+++ Mono.TextTemplating/Mono.VisualStudio.TextTemplating.VSHost/ITransformationRunFactoryService.cs
@@ -7,5 +7,10 @@ namespace Mono.VisualStudio.TextTemplating.VSHost
 	public interface ITransformationRunFactoryService
 	{
 		IProcessTransformationRunFactory GetTransformationRunFactory (Guid ID);
+		/// <summary>
+		/// Let the service now it can shutdown
+		/// </summary>
+		/// <returns></returns>
+		bool Shutdown ();
 	}
 }
