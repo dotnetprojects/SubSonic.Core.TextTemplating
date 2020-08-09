cd1fb629dbb57b80e98e7413450b302146cb9e39
Kenneth Carter <kccarter32@gmail.com>
2020-08-09 09:46:11 -0600

    see if this works

--- Mono.TextTemplating/Mono.VisualStudio.TextTemplating.VSHost/TransformationRunFactory.cs
+++ Mono.TextTemplating/Mono.VisualStudio.TextTemplating.VSHost/TransformationRunFactory.cs
@@ -15,7 +15,6 @@ namespace Mono.VisualStudio.TextTemplating.VSHost
 		[NonSerialized]
 		public readonly static ConcurrentDictionary<Guid, IProcessTransformationRunner> Runners = new ConcurrentDictionary<Guid, IProcessTransformationRunner> ();
 #endif
-		[NonSerialized]
 		readonly IProcessTextTemplatingEngine engine;
 
 
